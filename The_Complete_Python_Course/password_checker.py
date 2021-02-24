31d3c79c1e4ad6a5e956e8b0174ebc9851c70228
Sreyeesh <Sgarime1@gmail.com>
2021-02-24 03:13:48 +0000

    updates to password checker

--- The_Complete_Python_Course/password_checker.py
+++ The_Complete_Python_Course/password_checker.py
@@ -1,43 +1,36 @@
 """
 project: making password checker
 author: Sreyeesh Garimella
+
 """
 
-# user_name = input('Hello What is your name ? ')
-# user_password = input('Please input your password')
-#
-# print('Thank you for inputting your user name and password ')
-# show_password = input('would you like to show your password ? ')
-# if show_password == 'yes':
-#     print(show_password)
-# for password in user_password:
-#     print()
-# if user_password in user_password:
-#     password = []
-#     print('password has been stored ')
-#
-# show_password = input('Would you like to see  your password? Please type yes or no')
-#
-# answer_one = 'yes'
-# answer_no = 'no'
-#
-# if answer_one in show_password:
-#     print(password)
-# if answer_no in show_password:
-#     print('password will not be shown')
+user_name = input('please enter your name: ')
+user_list = []
+
+if user_name not in user_list:
+    print(f'Hello {user_name},welcome!')
+elif len(user_name) > 0: # this is to calculate the length  of the list when users get added
+    user_list.append(user_name)
+
+print(f'thank you for entering your name please set your password next')
+
+enter_password = input(str('please enter your password : '))
+user_password = []
+
+if enter_password in user_password:
+    user_password.append(enter_password)
+print(f'thank you for entering your password ')
+
+show_password = input(str(f'would you like to see your password : yes or no ?  '))
+answer_no = 'no'
+answer_yes = 'yes'
+
+if answer_no in show_password:
+    print('password will not be shown')
+
+if answer_yes in show_password:
+    if enter_password not in user_password:
+        print('this is your password',enter_password)
 
 
-user_list = ["Tom", "Jose", "Anna"]
 
-username = input("Please enter your name: ")
-
-if username in user_list:
-    print(f"Hi {username}! Welcome back")
-elif len(username) > 0:
-    user_list.append(username)
-    print(f"Hi {username}! Welcome to the club!")
-
-print("\n\nHere's a list of our users:")
-
-for user in user_list:
-    print(user)
