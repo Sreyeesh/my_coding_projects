484981b2fc1ac9ca2ee0da32875db05cb0a04819
Sreyeesh <Sgarime1@gmail.com>
2021-02-25 18:49:54 +0000

    updates to password_checker

--- The_Complete_Python_Course/password_checker.py
+++ The_Complete_Python_Course/password_checker.py
@@ -7,22 +7,13 @@ author: Sreyeesh Garimella
 user_name = input('please enter your name: ')
 user_list = []
 
-
-if user_name not in user_list:
-    print(f'Hello {user_name},welcome!')
-elif len(user_name) > 0: # this is to calculate the length  of the list when users get added
-    user_list.append(user_name)
-
-print(f'thank you for entering your name please set your password next')
-
 enter_password = input('please enter your password : ')
 user_password = []
 
 if enter_password  in user_password:
     user_password.append(enter_password)
 
-print(f'thank you for entering your password ')
-
+print(f'thank you for entering your name please set your password next')
 
 show_password = input(f'would you like to see your password : yes or no ?  ')
 
@@ -31,4 +22,11 @@ if show_password == 'no':
 else:
     print('this is your password',enter_password)
 
+
+
+
+#TODO: Iterate through the user list to show users that have signed up
+
+
+#TODO: have users reset password
 
