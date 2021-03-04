93a1cd5f1e6e6ca39ee7cd079f0109e153e766ec
Sreyeesh <Sgarime1@gmail.com>
2021-03-04 22:06:53 +0000

    updaets

--- The_Complete_Python_Course/simple_age_calculator.py
+++ The_Complete_Python_Course/simple_age_calculator.py
@@ -12,4 +12,17 @@ user_age = input('What is your age: ')
 
 user_age_number = int(user_age)
 
+seconds = user_age_number * (365 * 24 * 60 * 60)
+
 print(f'you have lived for {user_age_number * 12 } months.')
+
+print(f' you lived for {seconds} seoncds so far.')
+
+#
+# name = input("Enter your name: ")
+# print(f"Hello, {name}.")
+#
+# age = int(input("Enter your age: "))
+#
+# seconds = age * (365 * 24 * 60 * 60)
+# print(f"You lived for {seconds} seconds so far.")
