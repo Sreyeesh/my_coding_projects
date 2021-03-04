081a6c90b8bd12b853c12d3a41838a909fd4bed2
Sreyeesh <Sgarime1@gmail.com>
2021-03-04 21:04:34 +0000

    updates

--- The_Complete_Python_Course/simple_age_calculator.py
+++ The_Complete_Python_Course/simple_age_calculator.py
@@ -3,10 +3,12 @@ project : simple age calculator
 author : Sreyeesh
 '''
 
-# user_name = input('what is your name ? ')
-#
-# age = int(input('Enter your age: '))
-# print(f'Hello {user_name},you have lived for {age *12} months ')
+user_name = input(' What is your name ? ')
 
+print(f'Hello, {user_name}')
 
-user_name  = input('what is your name ? ')
+user_age =  input('What is your age: ')
+
+user_age_number = int(user_age)
+
+print(f'you have lived for {user_age_number * 12}months.')
