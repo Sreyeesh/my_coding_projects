cc128ac0975374b5045343d164ba3736fe42297c
Sreyeesh <Sgarime1@gmail.com>
2021-03-07 00:23:12 +0000

    updates

--- The_Complete_Python_Course/shopping_cart.py
+++ The_Complete_Python_Course/shopping_cart.py
@@ -7,3 +7,7 @@ Author : Sreyeesh Garimella
 '''
 
 print('Hello Welcome to Online Shopping List..')
+
+shopping_list = []
+
+add_to_shopping_list = input('would you like add to things to your shopping list ? ')
