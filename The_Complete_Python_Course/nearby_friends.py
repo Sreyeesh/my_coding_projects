170ac71794eedc7cb4448896cc153f2c112066f2
Sreyeesh Garimella <sgarime1@gmail.com>
2021-04-23 12:44:09 -0700

    updates

--- The_Complete_Python_Course/nearby_friends.py
+++ The_Complete_Python_Course/nearby_friends.py
@@ -18,12 +18,8 @@ friend_name = input('What is your friends name ? ')
 
 # Add the name to the empty set
 
-if friend_name in user_friends:
-    print(friend_name)
-else:
-    user_friends.add(friend_name)
+user_friends.add(friend_name)
 
-print(f'these are the friends nearby',user_friends)
     
  # Print out the intersection between both sets. This gives us a set with those friends that are nearby.
 print("nearby_people intersection of user_friends: ",user_friends.intersection(nearby_people))
