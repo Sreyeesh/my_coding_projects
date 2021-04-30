66e8664233c3748886e58bf8f43eb5b9bd8b0ac1
Sreyeesh Garimella <sgarime1@gmail.com>
2021-04-29 18:36:14 -0700

    finished panagram

--- The_Complete_Python_Course/detect_pangram.py
+++ The_Complete_Python_Course/detect_pangram.py
@@ -5,3 +5,20 @@ For example, the sentence "The quick brown fox jumps over the lazy dog" is a pan
 Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
 
 """
+ 
+        
+a_z = "abcdefghijklmnopqrstuvwxyz"
+
+panagram = input(str('please input a panagram : '))
+
+def panagram_phrase(a_z,panagram):
+    for letter in a_z: 
+        # print(f'these are the letters in the alphabet: ',letter)
+        if letter in panagram: 
+            print(f'these are the letters in the panagram: ', letter,panagram)
+            
+        else:
+            print('the letters are not in the panagram: ', letter.upper())
+        
+    
+panagram_phrase(a_z,panagram)
