62b19c66b7f2c471952f004b7b07d3a9240d8a3e
Sreyeesh <Sgarime1@gmail.com>
2021-05-13 23:26:17 -0700

    updates

--- The_Complete_Python_Course/Opposites_Attract.py
+++ The_Complete_Python_Course/Opposites_Attract.py
@@ -4,33 +4,34 @@ Timmy & Sarah think they are in love, but around where they live, they will only
 
 Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
 
-"""
+ """
 
-flower1 =list(range(10))
+flower1 = range(1,101)
+flower2 = range(5,201)
 
-#don't have two lists  with identical numbers try to use one list 
+def lovfunc(flower1,flower2):
+   pass 
 
-flower2 = list(range(1,11))
+    
+      
 
-
-def lovefunc( flower1, flower2 ):
-    for p in flower1 and flower2: #fix the for loop 
-        # print(f'this the number of even petals in flower 1:',p)
-        
-        if(p %2 ) == 0:
-            print(f'petal in flower1 is even',p)
-            print('they are in love')
-            # return True
-            
-        else:
-            print(f'petal in flower2  is odd',p) 
-            # return False
-           
-               
-        if(p %2 )  != 0:
-            print(f'they are not in love',p)
-            # return False
-
-lovefunc(flower1,flower2)
+lovfunc(flower1,flower2)
 
 
+# import random
+
+# tims_pick = random.randint(1, 100)
+# sarahs_pick = random.randint(1, 100)
+
+# def love_func(flower1, flower2):
+#     f1 = flower1 % 2 == 0
+#     f2 = flower2 % 2 == 0
+    
+#     if (f1 and not f2) or (not f1 and f2):
+#         # they're in-love, return True
+#         return True
+#     # not in love, return False
+#     return False
+
+# print('tims ->', tims_pick, 'sarahs ->', sarahs_pick)
+# print(love_func(tims_pick, sarahs_pick))
