bc15affc4212cc84d67b695271bf3727b05630f8
Sreyeesh Garimella <sgarime1@gmail.com>
2021-05-26 19:45:23 -0700

    updates

--- The_Complete_Python_Course/Opposites_Attract.py
+++ The_Complete_Python_Course/Opposites_Attract.py
@@ -8,48 +8,20 @@ Write a function that will take the number of petals of each flower and return t
 
 #picked a flower each
 flower1 =  range(1,101)
-flower2 =  range(1,100)
+flower2 =  range(34,580)
 
 def lovefunc(flower1,flower2):
     
-    # for petal in flower1:
-                 
-    #     if petal % 2 == 0:
-    #         print('even petals on flower 1: ',petal)
-    #     else: 
-    #         print('odd petals in flower 1: ',petal)
-    
-           
-    # for petal in flower2: 
-        
-    #     if petal % 2 == 0: 
-    #         print('even list of petal on flower 2: ', petal)        
-    #     else: 
-    #         print('odd petals in flower 2: ', petal)
-    
-            
-    # return (flower1 + flower2) % 2 == 1
-
-    
-            
-            
-    # flower_1 = flower1 % 2 == 0
-    # flower_2 = flower2 % 2 == 0
-    
-    # if(flower_1 and not flower_2) or (not flower_1 and flower_2):
-    #             return True  # They are in Love
-      
-    # return False  # They are not in False
-    
+ 
     for petal1 in flower1:
         for petal2 in flower2:
             if petal1 % 2 == 0 and petal2 % 2 == 1  or petal2 % 2 == 0 and petal1 % 2 ==1: 
                 return True
-                # print('flower one petals picked ',petal1,' petals pick  ', petal2,'Timmy and Sarah are in love')
+                print('flower one petals picked ',petal1,' petals pick  ', petal2,'Timmy and Sarah are in love')
                 
             # else:
             return False
-            # print('flower one petals picked ',petal1,' petals pick  ', petal2,'Timmy and Sarah are not  in love')
+            print('flower one petals picked ',petal1,' petals pick  ', petal2,'Timmy and Sarah are not  in love')
                 
     
 print(lovefunc(flower1,flower2))     
--- The_Complete_Python_Course/nearby_friends.py
+++ The_Complete_Python_Course/nearby_friends.py
@@ -8,18 +8,17 @@ Date: 4/22/2021
 
 """
 
-nearby_people = {'Rolf','Jen', 'Anna'}
-user_friends = set()
+nearby_people = {'Rolf', 'Jen', 'Anna'}
+user_friends = set()  # This is an empty set, like {}
 
-
-#Ask the user for the name of a friend 
+# Ask the user for the name of a friend
 
 friend_name = input('What is your friends name ? ')
 
 # Add the name to the empty set
-
 user_friends.add(friend_name)
 
- # Print out the intersection between both sets. This gives us a set with those friends that are nearby.
-print("nearby_people intersection of user_friends: ",user_friends.intersection(nearby_people))
+# Print out the intersection between both sets. This gives us a set with those friends that are nearby.
+print(user_friends.intersection(nearby_people))
+   
    
