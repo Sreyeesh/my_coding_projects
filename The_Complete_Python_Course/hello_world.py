a58dc12d4c3149c6ce8c3974280e1413562855bd
Sreyeesh Garimella <sgarime1@gmail.com>
2021-05-26 23:49:46 -0700

    studying

--- The_Complete_Python_Course/hello_world.py
+++ The_Complete_Python_Course/hello_world.py
@@ -1,1 +1,22 @@
-print("Hello World !")
+import os 
+
+# os.system('clear')
+
+#add list to the friends who want to join in the coffee shop 
+
+# first_name = input(' name of user ')
+
+# age = input('age of user ')
+
+
+list_of_friends = ['Ross', 'Rachel','Monica','Chandler', 'Joey','Phoebe']
+
+list_of_friends.append(first_name)
+len(list_of_friends)
+
+
+
+
+
+
+print(list_of_friends[0:4])
