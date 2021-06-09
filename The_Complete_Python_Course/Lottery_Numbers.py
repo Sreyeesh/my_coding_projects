f0dc2491f1203711389174c70c2f9b3fa0dc20ea
Sreyeesh <Sgarime1@gmail.com>
2021-06-08 20:44:26 -0700

    updates

--- The_Complete_Python_Course/Lottery_Numbers.py
+++ The_Complete_Python_Course/Lottery_Numbers.py
@@ -16,17 +16,7 @@ players = [{'name': 'Ross',
            ]
 
 
-<<<<<<< HEAD
-for player in players: 
-    player_one_name = players[0]['name']
-    player_two_name = players[1]['name']
-    numbers_correct = player['number'].intersection(lottery_numbers)
-    print(f" {player_one_name} got {len(numbers_correct)} numbers right.")
-    print(f" {player_two_name} got {len(numbers_correct)} numbers right.")
-    print('Hello,{}.You matched these numbers {} right'.format(player['name'],numbers_correct))
   
-=======
 for player in players:
     numbers_correct = player['numbers'].intersection(lottery_numbers)
     print('Player {} got {} numbers right.'.format(player['name'],len(numbers_correct)))
->>>>>>> fd10d15f1905bcc2e2a7a669e3d99b2a37cc8585
