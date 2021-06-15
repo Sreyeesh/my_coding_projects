c60430fce54e6d0441ab12dda78f306a62718fe7
Sreyeesh <Sgarime1@gmail.com>
2021-06-15 00:07:57 -0700

    added new script

---
+++ The_Complete_Python_Course/family_and_friends.py
@@ -0,0 +1,23 @@
+"""
+Project: Python Program to see whether family or friends
+
+Author : Sreyeesh Garimella
+
+Date: 6/14/21
+
+
+"""
+
+print(' Hello Harry Potter These are your family and friends: ')
+
+friends = ["Ron" , "Hermione"]
+family = ["Lilly","James"]
+
+user_name = input("Enter your name: ")
+
+if user_name in friends: 
+    print(user_name)
+else: 
+    print ("you are family")
+
+
