b0ee9c3f7548934045380a312d6f52072dfe2f23
Sreyeesh Garimella <sgarime1@gmail.com>
2021-06-17 00:37:20 -0700

    updaets to final force

--- The_Complete_Python_Course/while_why.py
+++ The_Complete_Python_Course/while_why.py
@@ -1,12 +1,16 @@
+
+    
+the_force = input('Do you believe you in the force ? (yes/no): ')
+
 print('May the force  be with you !')
 
-while True:
-    
-    the_force = input('Do you believe in the force ? (yes/no): ')
-    
-    if the_force == "yes":
-        print("The force will guide you !")
+while the_force:
 
+    the_force = input('Do you believe in the force ? (yes/no): ')
+     
+    if the_force == "yes": 
+        print("The force will guide you !")
     else:
         print('If Once You Start Down The Dark Path, Forever Will it Dominate Your Desitny!')
+    break
     
