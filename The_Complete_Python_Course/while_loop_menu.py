bfc4cfcdb84ba4f6f92573cb25f04c049ee75fce
Sreyeesh <Sgarime1@gmail.com>
2021-06-23 00:05:56 -0700

    starting to practice gui

--- The_Complete_Python_Course/while_loop_menu.py
+++ The_Complete_Python_Course/while_loop_menu.py
@@ -1,6 +1,6 @@
 user_menu = input('please enter p or q: ')
 
-while user_menu != 'q':  #run the code below as long as the user doesn't hit the letter q  that's what !=
+while user_menu != 'q':  #run the code below as long as the user doesn't hit the letter q  that's what != means
 
     if user_menu == 'p':
 
