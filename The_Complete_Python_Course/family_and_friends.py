fa25be68de1015e880182e8c3c97e3cfa4fae7fb
Sreyeesh <Sgarime1@gmail.com>
2021-06-22 19:57:55 -0700

    new script

--- The_Complete_Python_Course/family_and_friends.py
+++ The_Complete_Python_Course/family_and_friends.py
@@ -7,17 +7,3 @@ Date: 6/14/21
 
 
 """
-
-print(' Hello Harry Potter These are your family and friends: ')
-
-friends = ["Ron" , "Hermione"]
-family = ["Lilly","James"]
-
-user_name = input("Enter your name: ")
-
-if user_name in friends: 
-    print(user_name)
-else:
-    print ("you are a death eater")
-
-
