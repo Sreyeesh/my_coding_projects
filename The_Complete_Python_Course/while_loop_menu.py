0a883ba6501a9e3643a88dd2fb69d98df89ef358
Sreyeesh <Sgarime1@gmail.com>
2021-06-22 23:08:23 -0700

    finished while loop exercise

--- The_Complete_Python_Course/while_loop_menu.py
+++ The_Complete_Python_Course/while_loop_menu.py
@@ -1,24 +1,13 @@
-# Ask the user to choose one of two options:
+user_menu = input('please enter p or q: ')
 
-# if they type 'p', our program should print "Hello" and then ask the user again. Keep repeating this until...
+while user_menu != 'q':
 
-# if they type 'q', our program should terminate.
+    if user_menu == 'p':
 
-# Let's begin by asking the user to type either 'p' or 'q'. You know how to do this using input()
+        print('Hello')
+    else:
+        print('wrong selection')
 
+    user_menu = input('please enter p or q to continue:')
 
-# user_input = ...
-
-
-# Then, begin a while loop that runs for as long as the user doesn't type 'q'.
-
-
-
-
-# Inside the loop, have an if statement that checks if the user typed 'p'.
-
-#    If they did, print "Hello"
-
-# Still inside the loop, ask the user again
-
-# user_input = ...
+print('quitting program')
