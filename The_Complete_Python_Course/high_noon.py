7054c80219961f18bba0224e0e1685d1a57c3526
Sreyeesh Garimella <sgarime1@gmail.com>
2021-07-04 02:50:00 -0700

    updates to comment

--- The_Complete_Python_Course/high_noon.py
+++ The_Complete_Python_Course/high_noon.py
@@ -14,7 +14,7 @@ import pytz
 
 
 today_time = time.ctime()
-world_timezones = pytz.all_timezones #this is already list all time zones and not the actual time. 
+world_timezones = pytz.all_timezones #this is already list of  all time zones and not the actual time. 
 
 
 
