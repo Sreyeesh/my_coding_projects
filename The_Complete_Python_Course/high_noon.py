731f86c1fdfe4539838adec7eb45ed8e40e08faa
Sreyeesh <Sgarime1@gmail.com>
2021-07-05 02:26:04 -0700

    testing

--- The_Complete_Python_Course/high_noon.py
+++ The_Complete_Python_Course/high_noon.py
@@ -12,13 +12,17 @@ from datetime import date, datetime
 import time
 import pytz
 
-
+utc = pytz.utc
 today_time = time.ctime()
 world_timezones = pytz.all_timezones #this is already list of  all time zones and not the actual time. 
 
 
 
 
+
+# print(world_timezones)
+# print(today_time)
+# print(utc)
 
 
 
