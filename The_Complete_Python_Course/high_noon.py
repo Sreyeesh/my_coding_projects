f598fa8e609aace4bf47f2dbdf0fed82d1d6c351
Sreyeesh <Sgarime1@gmail.com>
2021-07-12 22:39:01 -0700

    updates

--- The_Complete_Python_Course/high_noon.py
+++ The_Complete_Python_Course/high_noon.py
@@ -3,7 +3,7 @@ Mcree: High Noon
 
 Author : Sreyeesh Garimella 
 
-Get the time when it's high noon somewhere in the world.
+Get the time when it's high noon somewhere in the world. This is fan made app for Overwatch !
 
 
 """
@@ -15,11 +15,23 @@ import time
 from playsound import playsound
 
 
+
 utc = pytz.utc
 
 # this is already list of  all time zones and not the actual time.
 
 world_timezones = pytz.all_timezones
+# now = datetime.now()
+
+now = datetime.now()
+the_time = now.strftime("%H:%M %p")
+print("Current Time = ",the_time)
+
+for t in the_time:
+    if t in the_time == "12:00 PM":
+       print(now)
+       playsound('/Users/sreyeeshgarimella/Documents/my_coding_projects/The_Complete_Python_Course/high_noon.mp3') 
+      
 
 # print(len(world_timezones))
 # print(world_timezones[0:359])
@@ -27,36 +39,21 @@ world_timezones = pytz.all_timezones
 
 for t in world_timezones[0:359]:
     print(t)
-    # print(datetime.now(pytz.timezone(t)).strftime('%a %d %b %Y %I:%M:%S.%f %p %z'))
-    
-    # if datetime.now(pytz.timezone(t).strftime('%H:%M %p')) ==  '12:00 PM': 
-    #     print(f'In {t} it\s high noon.')
     current_time = datetime.now(pytz.timezone(t)).strftime('%H:%M %p')
     
-    if current_time in world_timezones[0:35] == '12:00 PM':
+    if current_time in world_timezones[0:359] == '12:00 PM':
         playsound('/Users/sreyeeshgarimella/Documents/my_coding_projects/The_Complete_Python_Course/high_noon.mp3')
         # print("It\'s {t} high noon.")
         print(f"In {t} it\'s {current_time} high noon.")
-        
-        
+        break
+   
     else:
         # print("It ain\'t my  time.")
         playsound('/Users/sreyeeshgarimella/Documents/my_coding_projects/The_Complete_Python_Course/My_Time.mp3')
         print(f"In {t} ain\'t {current_time} my time")
-        break
+    
         
         # print('prepare to attack.')
-
     
-   
-        
-
-        
-        
-        
-        
-        
     
-  
-        
-  
+ 
